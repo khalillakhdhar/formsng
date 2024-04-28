557e010270a8a92486101764a8d9d9aa0ab5e6c5
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2024-04-28 12:03:24 +0100

    app

--- src/app/division/division.component.ts
+++ src/app/division/division.component.ts
@@ -8,9 +8,13 @@ import { Component } from '@angular/core';
 export class DivisionComponent {
   a:number=0;
   b:number=1;
+  divisibile?:string;
+  etat:boolean=false;
+  
   //x=7
   divisibilite(){
-    return this.a%this.b==0;
+  this.etat= this.a%this.b==0;
+    this.divisibile=this.etat?"divisible":"non divisible";
 
   }
 
